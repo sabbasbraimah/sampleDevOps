0bd268d7bc12edd68993603032900ae8e6c2c1d9
Seidu Abbas <braimah_abbas@yahoo.co.uk>
2021-01-02 23:22:42 +0000

    test change

--- single-module/src/test/java/com/example/TestGreeter.java
+++ single-module/src/test/java/com/example/TestGreeter.java
@@ -31,7 +31,15 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
-    @Test
+  @Test
+  public void greetShouldIncludeGreetingPhrase2() {
+    String someone = " Hello World, thanks for making me happy";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+
+
+  @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
 
